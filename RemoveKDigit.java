8414cb1bb4611f69ae49e43be383d4a10c9d9471
Sanjay P Lal <63526658+plalsanjay@users.noreply.github.com>
2020-05-15 09:05:29 +0530

    Create RemoveKDigit.java

---
+++ RemoveKDigit.java
@@ -0,0 +1,88 @@
+/*Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+Note:
+The length of num is less than 10002 and will be ≥ k.
+The given num does not contain any leading zero.
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.*/
+
+
+class Solution {
+    
+    public String removeKdigits(String num, int k) {
+        if(k==num.length())
+        {
+            return "0";
+        }
+        String result = num;
+        boolean change=true;
+        //int j=0;
+        for(int i=0;i<k;i++)
+        {
+            int j=0;
+            while(change)
+            {
+                if(j>result.length())
+                {
+                    break;
+                }
+                if(j==result.length()-1)
+                {
+                    result = result.substring(0, j) + result.substring(j+1);
+                    break;
+                }
+                //System.out.println("hello"+result);
+                //System.out.println(j);
+                //System.out.println(result.length());
+                
+                
+                int a=Integer.parseInt(String.valueOf(result.charAt(j)));
+                int b=Integer.parseInt(String.valueOf(result.charAt(j+1)));
+                //System.out.println("a is "+a+" b is "+b);
+                //System.out.println(j);
+                if(a>b)
+                {
+                    result = result.substring(0, j) + result.substring(j+1);
+                    break;
+                }
+                
+                //System.out.println("a is "+a+" b is "+b);
+                j++;
+                
+                //break;
+            }
+            //System.out.println(result);
+        }
+        //System.out.println(result);
+        int i=0;
+        while(result.charAt(i)=='0')
+        {
+            if(result.length()==1)
+            {
+                break;
+            }
+            
+            result = result.substring(0, i) + result.substring(i+1);
+            
+            //i++;
+            
+            //i++;
+        }
+        //System.out.println(result);
+        return result;
+        
+    }
+}
